4c89057ac370bb2984ef3c4bdc6e6ca77eb08d02
mikevar <lam.mikevar@gmail.com>
2026-05-03 18:02:15 +0700

    feat(drizzle-data-grid): make filterOperators dictionary

---
+++ packages/drizzle-data-grid/src/filter-operators.ts
@@ -0,0 +1,37 @@
+import type { FieldSchemaColumn, FilterOperator } from "./types.ts";
+import {
+  and,
+  or,
+  eq,
+  ilike,
+  gt,
+  gte,
+  lt,
+  lte,
+  isNull,
+  isNotNull,
+  inArray,
+  type SQL,
+  type AnyColumn,
+  between,
+} from "drizzle-orm";
+
+export const filterOperators: Record<
+  FilterOperator,
+  (col: FieldSchemaColumn, values: (string | number | boolean | Date)[]) => SQL
+> = {
+  eq: (col, [val]) => eq(col, val),
+
+  iLike: (col, [val]) => ilike(col as AnyColumn, `%${val}%`),
+
+  gt: (col, [val]) => gt(col, val),
+  gte: (col, [val]) => gte(col, val),
+  lt: (col, [val]) => lt(col, val),
+  lte: (col, [val]) => lte(col, val),
+
+  between: (col, [val1, val2]) => between(col, val1 as string, val2 as string),
+
+  isNull: (col, [val]) => (val === true ? isNull(col) : isNotNull(col)),
+
+  inArray: (col, values) => inArray(col, values),
+};
