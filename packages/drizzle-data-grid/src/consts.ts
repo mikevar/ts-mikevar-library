6c4ce1bf1b7bba579a23bc208c625409be662e3b
mikevar <lam.mikevar@gmail.com>
2026-05-23 11:55:36 +0700

    feat(drizzle-data-grid): add default value of query keys

--- packages/drizzle-data-grid/src/consts.ts
+++ packages/drizzle-data-grid/src/consts.ts
@@ -3,6 +3,14 @@ import type { FilterMode, PaginationMode } from "./types.ts";
 export const COL_DIRECTION_SEPARATOR = ":";
 export const COL_OPERATOR_SEPARATOR = "__";
 
+export const DEFAULT_FILTER_MODE_KEY = "filterMode";
+export const DEFAULT_SEARCH_KEY = "search";
+export const DEFAULT_PAGINATION_MODE_KEY = "paginationMode";
+export const DEFAULT_PAGE_KEY = "page";
+export const DEFAULT_LIMIT_KEY = "limit";
+export const DEFAULT_CURSOR_KEY = "cursor";
+export const DEFAULT_ORDERS_KEY = "orders";
+
 export const DEFAULT_PAGINATION_MODE: PaginationMode = "offset";
 export const DEFAULT_PAGE = 1;
 export const DEFAULT_LIMIT = 10;
