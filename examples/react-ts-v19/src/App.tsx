c978af1cac9266fb864bad6f674ca23262db3f97
mikevar <lam.mikevar@gmail.com>
2026-05-26 18:01:23 +0700

    example(react-data-grid): make simple example

--- examples/react-ts-v19/src/App.tsx
+++ examples/react-ts-v19/src/App.tsx
@@ -1,7 +1,28 @@
+import { useDataGridStates } from "@mikevar/react-data-grid";
+
 function App() {
+  const { persistedState, draftState, actions } = useDataGridStates({
+    url: "?id_a=1&id_b=2&id_c=3&filterMode=filter&search=something&page=5&limit=6&cursor=7&orders=id:asc",
+    filterQueryKeys: ["name__iLike"],
+    defaultQueryValues: {},
+    onUrlChange(url) {
+      console.log(url);
+    },
+  });
+
   return (
     <>
       <div>Hello World</div>
+      <pre>{JSON.stringify({ persistedState, draftState }, null, 2)}</pre>
+      <input
+        value={draftState.filtering.search}
+        onChange={(e) => actions.setSearch(e.target.value)}
+      />
+      <input
+        value={draftState.filtering.rawFilters["name__iLike"]}
+        onChange={(e) => actions.setFilter("name__iLike", e.target.value)}
+      />
+      <button onClick={actions.submit}>Submit</button>
     </>
   );
 }
